34af73a2615a90bcb2f549f0b2c698a2716ab730
Kjel Delaey <213738+trimentor@users.noreply.github.com>
2020-09-22 20:21:49 +0200

    Process .js(x) files
    Based on Babel's functionality, webpack needs to know that .js and .jsx
    files need to go through Babel before being bundled.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,17 @@ module.exports = {
   },
   entry: './src/main.js',
   mode: NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
